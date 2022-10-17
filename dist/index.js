c6f428d86a74456f1b3dfcedf10ce1b565febe60
Jaime Andres Velez Osorio <javobqcol@gmail.com>
2022-10-17 09:30:36 -0500

    borrar

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-var _app = _interopRequireDefault(require("./app"));
-require("./database");
-var _config = require("./config");
-_app["default"].listen(_config.PORT);
-console.log("server on port ".concat(_config.PORT));
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import app from "./app"
-import './database'
-import { PORT } from './config'
-
-
-app.listen(PORT)
-console.log( `server on port ${PORT}`)
